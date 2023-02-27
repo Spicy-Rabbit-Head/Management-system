5d51ebbffbed37fd70d86b38f6972d4110eacd23
Spicy-Rabbit-Head <1192469846@qq.com>
2023-02-27 14:35:59 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ Module-ResultCode/src/main/java/com/zzk/resultcode/SystemStatusCode.java
@@ -0,0 +1,17 @@
+package com.zzk.resultcode;
+
+/**
+ * 系统状态码
+ */
+
+public class SystemStatusCode {
+    /**
+     * 错误
+     */
+    int SYSTEM_USER_ERROR_ADD_FAIL = 0;
+
+    /**
+     * 提示
+     */
+    int SYSTEM_USER_INFO_ADD_SUCCESS = 1;
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Management-system
+# Manage the back-end of the system
 
 学习spring boot 构建管理系统后端 
