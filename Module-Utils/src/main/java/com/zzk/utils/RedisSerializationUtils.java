00249b39572892c7b23ab9f24887e6def557eacf
Spicy-Rabbit-Head <1192469846@qq.com>
2023-03-08 16:38:34 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Management-System-Entry/src/main/resources/application-devDB.yaml
    #	Management-System-Entry/src/main/resources/application-devMP.yaml
    #	Module-Dao/pom.xml
    #	Module-Entity/pom.xml
    #	Module-Entity/src/main/resources/application.properties
    #	Module-Security/pom.xml
    #	Module-Security/src/main/java/com/zzk/security/ModuleSecurityApplication.java
    #	Module-Security/src/main/resources/application.properties
    #	pom.xml

--- Module-Utils/src/main/java/com/zzk/utils/RedisSerializationUtils.java
+++ Module-Utils/src/main/java/com/zzk/utils/RedisSerializationUtils.java
@@ -1,10 +1,12 @@
 package com.zzk.utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * 基于 StringRedisTemplate 的 Redis 序列化工具类
@@ -38,24 +40,12 @@ public class RedisSerializationUtils {
     }
 
     /**
-     * 从 Redis 中获取字符串
-     *
-     * @param key 键
-     * @return 字符串值，如果不存在则返回 null
-     */
-    public String getString(String key) {
-        // 调用 StringRedisTemplate 的 get 方法，从 Redis 中获取字符串
-        return stringRedisTemplate.opsForValue().get(key);
-    }
-
-
-    /**
      * 将对象序列化为 JSON 字符串并存储到 Redis 中
      *
      * @param key   键
      * @param value 值（任意对象）
      */
-    public void setObject(String key, Object value) {
+    public void setString(String key, Object value) {
         try {
             // 将对象转换为 JSON 字符串
             String json = objectMapper.writeValueAsString(value);
@@ -67,13 +57,24 @@ public class RedisSerializationUtils {
     }
 
     /**
+     * 从 Redis 中获取字符串
+     *
+     * @param key 键
+     * @return 字符串值，如果不存在则返回 null
+     */
+    public String getString(String key) {
+        // 调用 StringRedisTemplate 的 get 方法，从 Redis 中获取字符串
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+
+    /**
      * 从 Redis 中获取 JSON 字符串并反序列化为对象
      *
      * @param key   键
      * @param clazz 值的类型（Class 对象）
      * @return 反序列化后的对象，如果不存在则返回 null
      */
-    public <T> T getObject(String key, Class<T> clazz) {
+    public <T> T getString(String key, Class<T> clazz) {
         try {
             // 调用 StringRedisTemplate 的 get 方法，从 Redis 中获取 JSON 字符串
             String json = stringRedisTemplate.opsForValue().get(key);
@@ -81,6 +82,77 @@ public class RedisSerializationUtils {
                 // 将 JSON 字符串转换为对象并返回
                 return objectMapper.readValue(json, clazz);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将 Map 集合存储到 Redis 中的 Hash 类型
+     *
+     * @param key 键
+     * @param map 值（Map 集合）
+     */
+    public void setHash(String key, Map<String, String> map) {
+        // 调用 StringRedisTemplate 的 opsForHash 方法，获取 HashOperations 对象
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        // 调用 putAll 方法，将 Map 集合存储到 Redis 中的 Hash 类型
+        hashOperations.putAll(key, map);
+    }
+
+    /**
+     * 将对象转换为 JSON 字符串后存储到 Redis 中的 Hash 类型
+     *
+     * @param key     键
+     * @param hashKey 哈希键
+     * @param object  值（对象）
+     */
+    public void setHash(String key, String hashKey, Object object) {
+        // 调用 StringRedisTemplate 的 opsForHash 方法，获取 HashOperations 对象
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        // 调用 put 方法，将对象转换为 JSON 字符串后存储到 Redis 中的 Hash 类型（键值对）
+        try {
+            // 将对象转换为 JSON 字符串
+            hashOperations.put(key, hashKey, objectMapper.writeValueAsString(object));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 从 Redis 中获取 Hash 类型的数据，并转换为 Map 集合
+     *
+     * @param key 键
+     * @return Map 集合，如果不存在则返回 null
+     */
+    public Map<String, String> getHash(String key) {
+        // 调用 StringRedisTemplate 的 opsForHash 方法，获取 HashOperations 对象
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        // 调用 entries 方法，从 Redis 中获取 Hash 类型的数据，并转换为 Map 集合
+        return hashOperations.entries(key);
+    }
+
+    /**
+     * 从 Redis 中获取 Hash 类型的数据，并转换为对象
+     *
+     * @param key     键
+     * @param hashKey 哈希键
+     * @param clazz   对象类型（Class 对象）
+     * @return 对象，如果不存在则返回 null
+     */
+    public <T> T getHash(String key, String hashKey, Class<T> clazz) {
+        // 调用 StringRedisTemplate 的 opsForHash 方法，获取 HashOperations 对象
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        // 调用 get 方法，从 Redis 中获取 Hash 类型的数据，并转换为 JSON 字符串
+        String json = hashOperations.get(key, hashKey);
+        // 判断 JSON 字符串是否为空或空字符串，如果是则返回 null
+        if (json == null || json.isEmpty()) {
+            return null;
+        }
+        try {
+            // 将 JSON 字符串转换为对象并返回
+            return objectMapper.readValue(json, clazz);
         } catch (IOException e) {
             e.printStackTrace();
         }
