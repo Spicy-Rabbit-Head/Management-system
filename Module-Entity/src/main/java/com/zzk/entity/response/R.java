adfcfa0ddee290ab991611ce677c8eeced200849
Spicy-Rabbit-Head <1192469846@qq.com>
2023-03-09 15:18:07 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Management-System-Entry/src/main/resources/application-devDB.yaml
    #	Management-System-Entry/src/main/resources/application-devMP.yaml
    #	Module-Dao/pom.xml
    #	Module-Entity/pom.xml
    #	Module-Entity/src/main/resources/application.properties
    #	Module-Security/pom.xml
    #	Module-Security/src/main/java/com/zzk/security/ModuleSecurityApplication.java
    #	Module-Security/src/main/resources/application.properties
    #	pom.xml

--- Module-Entity/src/main/java/com/zzk/entity/response/R.java
+++ Module-Entity/src/main/java/com/zzk/entity/response/R.java
@@ -3,6 +3,13 @@ package com.zzk.entity.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * 统一响应实体类
+ *
+ * @param <T> 响应数据类型
+ * @author zhaozikui
+ * @since 2021-03-05 16:00
+ */
 @Data
 @AllArgsConstructor
 public class R<T> {
