0bfb7add56c42233ab3f39a91026fa65731936f1
Spicy-Rabbit-Head <1192469846@qq.com>
2023-03-09 19:50:49 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Management-System-Entry/src/main/resources/application-devDB.yaml
    #	Management-System-Entry/src/main/resources/application-devMP.yaml
    #	Module-Dao/pom.xml
    #	Module-Entity/pom.xml
    #	Module-Entity/src/main/resources/application.properties
    #	Module-Security/pom.xml
    #	Module-Security/src/main/java/com/zzk/security/ModuleSecurityApplication.java
    #	Module-Security/src/main/resources/application.properties
    #	pom.xml

--- Management-System-Entry/src/main/java/com/zzk/entrance/ModuleEntranceApplication.java
+++ Management-System-Entry/src/main/java/com/zzk/entrance/ModuleEntranceApplication.java
@@ -1,16 +1,30 @@
 package com.zzk.entrance;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 模块入口
+ * <p>
+ * 1. @SpringBootApplication：标注一个主程序类，说明这是一个Spring Boot应用
+ * <p>
+ * 2. @ComponentScan：组件扫描，扫描所有的组件，包括配置类
+ * <p>
+ * 3. @MapperScan：扫描所有的Mapper接口
+ *
+ * @author zhaozikui
+ * @since 2021-02-20 19:01
+ */
+
 @SpringBootApplication
 // 组件扫描
 @ComponentScan(basePackages = "com.zzk")
+@MapperScan(basePackages = "com.zzk.dao")
 public class ModuleEntranceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ModuleEntranceApplication.class, args);
     }
-
 }
