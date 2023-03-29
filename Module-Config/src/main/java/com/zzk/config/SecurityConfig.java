2f1e032acba7efd2cf281fa9596d4396c60e07f5
Spicy-Rabbit-Head <1192469846@qq.com>
2023-03-29 17:00:52 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Management-System-Entry/src/main/java/com/zzk/entrance/ModuleEntranceApplication.java
    #	Management-System-Entry/src/test/java/com/zzk/entrance/daoTexts/UserDaoText.java
    #	Module-Utils/src/main/java/com/zzk/utils/JwtUtils.java
    #	Module-Utils/src/main/java/com/zzk/utils/RedisSerializationUtils.java

--- Module-Config/src/main/java/com/zzk/config/SecurityConfig.java
+++ Module-Config/src/main/java/com/zzk/config/SecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * Spring Security 配置类<br>
@@ -44,6 +47,18 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // 关闭跨域保护
+                .csrf().disable()
+                // 关闭session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+                .and().build();
     }
 
 }
